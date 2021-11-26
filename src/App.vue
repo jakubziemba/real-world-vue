af3a01e87d919fbadfde697137573e9bc6279faa
Jakub Ziemba <ziemba.jak@gmail.com>
2021-11-26 18:53:44 +0100

    feat/real-world-vue course files

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
-<template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view />
+<template lang="pug">
+#nav
+  router-link(:to='{ name: "EventList" }') Events
+  router-link(:to='{ name: "About" }') About
+router-view
 </template>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -17,14 +16,15 @@
 
 #nav {
   padding: 30px;
-}
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    margin: 0 10px;
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
---
+++ src/views/EventDetails.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+div(v-if='event')
+  h1 {{ event.title }}
+  p {{ event.time }} on {{ event.date }} @ {{ event.location }}
+  p {{ event.description }}
+</template>
+
+<script>
+import EventService from '@/services/EventService.js'
+
+export default {
+  props: ['id'],
+  data() {
+    return {
+      event: null,
+    }
+  },
+  created() {
+    EventService.getEvent(this.id)
+      .then((response) => (this.event = response.data))
+      .catch((error) => console.log(error))
+  },
+}
+</script>
